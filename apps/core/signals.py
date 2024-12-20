082a05e3d1ace841e84f77f97d33c4ea11a19a67
Vitalii Omelai <v.omelay@gmail.com>
2024-12-20 16:37:22 +0200

    Added signals.py

---
+++ apps/core/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from .models import Price
+
+
+@receiver(post_delete, sender=Price)
+def delete_related_item(sender, instance, **kwargs):
+    if instance.item:
+        instance.item.delete()
