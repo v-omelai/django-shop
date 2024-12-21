695321238ef455bac30c9d563ecb84d85194ff59
Vitalii Omelai <v.omelay@gmail.com>
2024-12-21 19:01:24 +0200

    Improved output for seller deletion

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -21,10 +21,31 @@ class PriceAdmin(admin.ModelAdmin):
         return deleted_objects, model_count, perms_needed, protected
 
 
+class SellerAdmin(admin.ModelAdmin):
+    def get_deleted_objects(self, objs, request):
+        deleted_objects, model_count, perms_needed, protected = super().get_deleted_objects(objs, request)
+        buyers = 0
+        buyer_inventories = 0
+        for obj in objs:
+            if obj.buyer:
+                link = reverse(
+                    f'admin:{obj.buyer._meta.app_label}_{obj.buyer._meta.model_name}_change',  # noqa
+                    args=[obj.buyer.pk]
+                )
+                html = mark_safe(f'Buyer: <a href="{link}">{obj.buyer}</a>')
+                buyers += 1
+                if obj.buyer.inventory:
+                    buyer_inventories += obj.buyer.inventory.count()
+                deleted_objects.append(html)
+        model_count['buyers'] = buyers
+        model_count['buyer inventories'] = buyer_inventories
+        return deleted_objects, model_count, perms_needed, protected
+
+
 admin.site.register(Item)
 admin.site.register(Price, PriceAdmin)
 admin.site.register(Buyer)
-admin.site.register(Seller)
+admin.site.register(Seller, SellerAdmin)
 admin.site.register(BuyerInventory)
 admin.site.register(SellerInventory)
 admin.site.register(Goal)
