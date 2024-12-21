bb0de7a11a937c0e7048d92ecd48a77eb7259ab5
Vitalii Omelai <v.omelay@gmail.com>
2024-12-21 19:00:24 +0200

    Added signal for seller deletion

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -1,9 +1,15 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from .models import Price
+from .models import Price, Seller
 
 
 @receiver(post_delete, sender=Price)
 def delete_related_item(sender, instance, **kwargs):
     if instance.item:
         instance.item.delete()
+
+
+@receiver(post_delete, sender=Seller)
+def delete_related_buyer(sender, instance, **kwargs):
+    if instance.buyer:
+        instance.buyer.delete()
