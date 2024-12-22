8ceb8ad03744f7935778f6202b12b78e7457bfd4
Vitalii Omelai <v.omelay@gmail.com>
2024-12-22 13:52:36 +0200

    Added loading page

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,14 +1,23 @@
+import random
+
 from django.views.generic import TemplateView
 
 from apps.core.models import *
 
 
 class GamePageView(TemplateView):
-    template_name = 'pages/game.html'
+    template_name = 'pages/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['buyer'] = Buyer.objects.last()
         context['seller'] = Seller.objects.last()
         context['goal'] = Goal.objects.last()
+        context['text'] = random.choice([
+            'Slicing onions...',
+            'Chopping carrots...',
+            'Boiling broccoli...',
+            'Juicing oranges...',
+            'Packing watermelons...',
+        ])
         return context
