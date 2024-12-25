e761d81fb622af926999220303de805534ed1044
Vitalii Omelai <v.omelay@gmail.com>
2024-12-25 17:30:37 +0200

    Find or create a game

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,20 +1,28 @@
 async function initiateGame() {
     try {
-        const response = await fetch('/api/game/', {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': window.getCookie('csrftoken'),
-                'Content-Type': 'application/json',
-            },
-        });
-        if (response.ok) {
-            const json = await response.json();
-            window.success(3);
-            await window.sleep(4);
-            window.location.href = json.redirect;
+        let redirect = localStorage.getItem('redirect');
+        if (redirect) {
+            console.log(`Found: ${redirect}`);
         } else {
-            throw new Error(response.status);
+            const response = await fetch('/api/game/', {
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': window.getCookie('csrftoken'),
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const json = await response.json();
+                redirect = JSON.stringify(json.redirect);
+                localStorage.setItem('redirect', redirect);
+                console.log(`Created: ${redirect}`);
+            } else {
+                throw new Error(response.status);
+            }
         }
+        window.success(3);
+        await window.sleep(4);
+        window.location.href = JSON.parse(redirect);
     } catch (error) {
         console.error(error.message);
         window.error(3);
