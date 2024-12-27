10ef79cc891d259259d65ba0428042276b8b8ce3
Vitalii Omelai <v.omelay@gmail.com>
2024-12-27 17:24:15 +0200

    Fixed 404

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,10 +1,18 @@
 async function initiateGame() {
     try {
-        let redirect = localStorage.getItem('redirect');
-        if (redirect) {
-            console.log(`Found: ${redirect}`);
+        let parsed;
+        let link = localStorage.getItem('link');
+        if (link) {
+            parsed = JSON.parse(link);
+            const response = await fetch(parsed);
+            if (response.status == 404) {
+                console.log(`Not Found: ${link}`);
+                localStorage.removeItem('link');
+                await initiateGame();
+                return;
+            }
         } else {
-            const response = await fetch('/api/game/', {
+            const response = await fetch(API_CREATE_SELLER_URL, {
                 method: 'POST',
                 headers: {
                     'X-CSRFToken': window.getCookie('csrftoken'),
@@ -13,16 +21,17 @@ async function initiateGame() {
             });
             if (response.ok) {
                 const json = await response.json();
-                redirect = JSON.stringify(json.redirect);
-                localStorage.setItem('redirect', redirect);
-                console.log(`Created: ${redirect}`);
+                parsed = json.link;
+                link = JSON.stringify(parsed);
+                localStorage.setItem('link', link);
+                console.log(`Created: ${link}`);
             } else {
                 throw new Error(response.status);
             }
         }
         window.success(3);
         await window.sleep(4);
-        window.location.href = JSON.parse(redirect);
+        window.location.href = parsed;
     } catch (error) {
         console.error(error.message);
         window.error(3);
