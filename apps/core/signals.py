340f80be4b6039c8fd465137c8c2e170c33482a4
Vitalii Omelai <v.omelay@gmail.com>
2024-12-27 17:59:12 +0200

    Fixed signals

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -1,15 +1,19 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from .models import Price, Seller
+from .models import Price, Seller, Item, Buyer
 
 
 @receiver(post_delete, sender=Price)
 def delete_related_item(sender, instance, **kwargs):
-    if instance.item:
+    try:
         instance.item.delete()
+    except Item.DoesNotExist:  # noqa
+        pass
 
 
 @receiver(post_delete, sender=Seller)
 def delete_related_buyer(sender, instance, **kwargs):
-    if instance.buyer:
+    try:
         instance.buyer.delete()
+    except Buyer.DoesNotExist:  # noqa
+        pass
