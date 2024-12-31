576ac4f1a52132041296d2a173310228ffce363b
Vitalii Omelai <v.omelay@gmail.com>
2024-12-31 13:57:58 +0200

    Moved logic from views to serializers. Is it good idea?

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,8 +1,9 @@
+from django.db import transaction
 from django.urls import reverse
 from drf_extra_fields.fields import Base64ImageField
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
-from .models import Seller
+from .models import *
 
 
 class CreateSellerSerializer(serializers.ModelSerializer):
@@ -14,8 +15,27 @@ class CreateSellerSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'link']
 
     @extend_schema_field(serializers.CharField)
-    def get_link(self, seller):  # noqa
-        return reverse('game', kwargs={'seller': seller.id})
+    def get_link(self, instance):  # noqa
+        return reverse('game', kwargs={'seller': instance.id})
+
+    def save(self):
+        with transaction.atomic():
+            # Initiate a buyer
+            buyer = Buyer.objects.create(name='Vitalii Omelai', balance=500)  # noqa
+
+            # Initiate a seller
+            goal = Goal.objects.get(difficulty=1)  # noqa
+            seller = self.instance = Seller.objects.create(name='Player', balance=0, buyer=buyer, goal=goal)  # noqa
+
+            # Add an apple
+            apple = Item.objects.get(name='Apple')  # noqa
+            SellerInventory.objects.create(seller=seller, quantity=1, item=apple)  # noqa
+
+            # Add a carrot
+            carrot = Item.objects.get(name='Carrot')  # noqa
+            SellerInventory.objects.create(seller=seller, quantity=1, item=carrot)  # noqa
+
+        return self.instance
 
 
 class UpdateSellerSerializer(serializers.ModelSerializer):
@@ -36,8 +56,49 @@ class ItemSerializer(serializers.Serializer):
     quantity = serializers.IntegerField()
 
 
-class CreateTransactionSerializer(serializers.Serializer):
-    buyer = serializers.CharField(max_length=36)
-    seller = serializers.CharField(max_length=36)
-    buy = ItemSerializer(many=True)
-    sell = ItemSerializer(many=True)
+class CreateTransactionSerializer(serializers.ModelSerializer):
+    buy = ItemSerializer(many=True, required=False)
+    sell = ItemSerializer(many=True, required=False)
+    processed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Transaction
+        fields = ['buyer', 'seller', 'buy', 'sell', 'processed']
+        read_only_fields = ['processed']
+
+    @extend_schema_field(serializers.BooleanField)
+    def get_processed(self, instance):  # noqa
+        return True if instance.status == 'completed' else False
+
+    def save(self):
+        validated_data = self.validated_data
+        buyer = validated_data.get('buyer')
+        seller = validated_data.get('seller')
+        print(validated_data)
+        t = self.instance = Transaction.objects.create(buyer=buyer, seller=seller)  # noqa
+        processed = False
+
+        with transaction.atomic():
+            t.status = 'completed'
+
+            buy = validated_data.get('buy', [])
+
+            sell = validated_data.get('sell', [])
+            for item in sell:
+                inventory = SellerInventory.objects.filter(  # noqa
+                    seller=seller,
+                    quantity__gte=item.get('quantity', 0),
+                    item__name=item.get('name', '').capitalize(),
+                )
+                if inventory.exists():
+                    [inventory] = inventory
+                    price = inventory.item.price.seller
+                    balance = buyer.balance
+                    processed = True
+                else:
+                    processed = False
+
+        t.status = 'completed' if processed else 'canceled'
+        t.save()
+
+        return self.instance
--- apps/core/views.py
+++ apps/core/views.py
@@ -2,14 +2,10 @@ import logging
 import random
 
 from django.core.exceptions import ValidationError
-from django.db import transaction
 from django.http import Http404
 from django.views.generic import TemplateView
-from rest_framework import status
 from rest_framework.generics import UpdateAPIView, CreateAPIView
-from rest_framework.response import Response
 
-from apps.core.models import *
 from apps.core.serializers import *
 
 
@@ -46,7 +42,7 @@ class GamePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         try:
-            seller = Seller.objects.get(id=kwargs.get('seller'))  # noqa
+            seller = Seller.objects.get(id=kwargs.get('seller', ''))  # noqa
             buyer = seller.buyer
 
             context.update({
@@ -67,30 +63,6 @@ class CreateSellerView(CreateAPIView):
     queryset = Seller.objects.all()  # noqa
     serializer_class = CreateSellerSerializer
 
-    def perform_create(self, serializer):
-        with transaction.atomic():
-            # Initiate a buyer
-            buyer = Buyer.objects.create(name='Vitalii Omelai', balance=500)  # noqa
-
-            # Initiate a seller
-            goal = Goal.objects.get(difficulty=1)  # noqa
-            seller = Seller.objects.create(name='Player', balance=0, buyer=buyer, goal=goal)  # noqa
-
-            # Add an apple
-            apple = Item.objects.get(name='Apple')  # noqa
-            SellerInventory.objects.create(seller=seller, quantity=1, item=apple)  # noqa
-
-            # Add a carrot
-            carrot = Item.objects.get(name='Carrot')  # noqa
-            SellerInventory.objects.create(seller=seller, quantity=1, item=carrot)  # noqa
-
-            return seller
-
-    def create(self, request, *args, **kwargs):
-        data = self.perform_create(None)
-        serialized = self.get_serializer(data)
-        return Response(serialized.data, status=status.HTTP_201_CREATED)
-
 
 class UpdateSellerView(UpdateAPIView):
     queryset = Seller.objects.all()  # noqa
@@ -102,9 +74,3 @@ class UpdateSellerView(UpdateAPIView):
 class CreateTransactionView(CreateAPIView):
     queryset = Transaction.objects.all()  # noqa
     serializer_class = CreateTransactionSerializer
-
-    def perform_create(self, serializer):
-        data = serializer.validated_data
-        buyer = Buyer.objects.get(id=data.get('buyer'))  # noqa
-        seller = Seller.objects.get(id=data.get('seller'))  # noqa
-        t = Transaction.objects.create(buyer=buyer, seller=seller)  # noqa
