bae2bdcb3a1e118754e6f29fc7216230f8276399
Vitalii Omelai <v.omelay@gmail.com>
2025-01-01 22:03:28 +0200

    Updated back-end logic

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from drf_extra_fields.fields import Base64ImageField
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
 
 from apps.core.exceptions import TransactionException
-from apps.core.models import Seller, Buyer, SellerInventory, Goal, Transaction, Item, BuyerInventory
+from apps.core.helpers import Helper
+from apps.core.models import Seller, Buyer, SellerInventory, Goal, Transaction, BuyerInventory
 
 
 class CreateSellerSerializer(serializers.ModelSerializer):
@@ -30,8 +30,8 @@ class CreateSellerSerializer(serializers.ModelSerializer):
             goal = Goal.objects.get(difficulty=1)  # noqa
             seller = self.instance = Seller.objects.create(name='Player', balance=0, buyer=buyer, goal=goal)  # noqa
 
-            # Create items
-            exec(goal.code)
+            # Create inventories
+            Helper.Inventory.create(buyer, seller, goal)
 
         return self.instance
 
@@ -50,18 +50,17 @@ class UpdateSellerSerializer(serializers.ModelSerializer):
 
 
 class ItemSerializer(serializers.Serializer):
-    OPERATION_CHOICES = [
-        ('buy', 'Buy'),
-        ('sell', 'Sell'),
-    ]
-
     name = serializers.CharField(max_length=255)
     quantity = serializers.IntegerField(min_value=1, default=1)
-    operation = serializers.ChoiceField(choices=OPERATION_CHOICES)
+
+
+class EntitySerializer(serializers.Serializer):
+    buyer = ItemSerializer(many=True, write_only=True, required=False)
+    seller = ItemSerializer(many=True, write_only=True, required=False)
 
 
 class CreateTransactionSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, write_only=True)
+    items = EntitySerializer(write_only=True)
     processed = serializers.SerializerMethodField()
     link = serializers.SerializerMethodField()
 
@@ -82,7 +81,7 @@ class CreateTransactionSerializer(serializers.ModelSerializer):
         validated_data = self.validated_data
         buyer = validated_data.get('buyer')
         seller = validated_data.get('seller')
-        items = validated_data.get('items', [])
+        items = validated_data.get('items', {})
         t = self.instance = Transaction.objects.create(buyer=buyer, seller=seller)  # noqa
 
         try:
@@ -90,39 +89,35 @@ class CreateTransactionSerializer(serializers.ModelSerializer):
                 raise TransactionException
 
             with transaction.atomic():
-                for dictionary in items:
-                    name = dictionary.get('name', '').capitalize()
-                    quantity = dictionary.get('quantity', 1)
-                    operation = dictionary.get('operation')
+                for dictionary in items.get('buyer', []):
+                    item, quantity = Helper.Item.get(dictionary)
+                    if BuyerInventory.objects.filter(  # noqa
+                            buyer=buyer, item=item, quantity__gte=quantity
+                    ).exists():
+                        seller.balance -= item.price.seller * quantity
+                    else:
+                        raise TransactionException
 
-                    item = get_object_or_404(Item, name=name)
-
-                    match operation:
-                        case 'buy':
-                            if BuyerInventory.objects.filter(  # noqa
-                                    buyer=buyer, item=item, quantity__gte=quantity
-                            ).exists():
-                                seller.balance -= item.price.seller * quantity
-                            else:
-                                raise TransactionException
-                        case 'sell':
-                            if SellerInventory.objects.filter(  # noqa
-                                    seller=seller, item=item, quantity__gte=quantity
-                            ).exists():
-                                seller.balance += item.price.buyer * quantity
-                            else:
-                                raise TransactionException
+                for dictionary in items.get('seller', []):
+                    item, quantity = Helper.Item.get(dictionary)
+                    if SellerInventory.objects.filter(  # noqa
+                            seller=seller, item=item, quantity__gte=quantity
+                    ).exists():
+                        seller.balance += item.price.buyer * quantity
+                    else:
+                        raise TransactionException
 
                 if seller.balance == seller.goal.balance:
                     goal = seller.goal = Goal.objects.filter(  # noqa
                         difficulty__gt=seller.goal.difficulty
                     ).order_by('difficulty').first()
 
-                    BuyerInventory.objects.filter(buyer=buyer).delete()  # noqa
-                    SellerInventory.objects.filter(seller=seller).delete()  # noqa
+                    # Delete inventories
+                    Helper.Inventory.delete(buyer, seller)
 
+                    # Create inventories
                     if goal:
-                        exec(goal.code)
+                        Helper.Inventory.create(buyer, seller, goal)
 
                     seller.save()
                 else:
